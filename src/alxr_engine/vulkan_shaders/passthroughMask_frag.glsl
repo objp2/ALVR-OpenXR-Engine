45387947b8c8b38172562fa32eb927660212a6d9
objp2 <objprototypee@pm.me>
2024-11-27 22:27:33 -0700

    Update passthroughMask_frag.glsl

--- src/alxr_engine/vulkan_shaders/passthroughMask_frag.glsl
+++ src/alxr_engine/vulkan_shaders/passthroughMask_frag.glsl
@@ -8,21 +8,25 @@
 #pragma fragment
 
 #include "common/baseVideoFrag.glsl"
+#include "common/color-functions.glsl" //added from Rachmanin0xF github
 
-layout(constant_id = 23) const float AlphaValue = 0.3f;
-layout(constant_id = 24) const float KeyColorR = 0.01f;
-layout(constant_id = 25) const float KeyColorG = 0.01f;
-layout(constant_id = 26) const float KeyColorB = 0.01f;
+vec3 RGB_TO_LAB(vec3 rgb) {
+    return XYZ_TO_LAB(RGB_TO_XYZ(rgb));
+}
 
-const vec3 key_color = vec3(KeyColorR, KeyColorG, KeyColorB);
+float when_lt(float x, float y) {
+    return max(sign(y - x), 0.0);
+}
 
 layout(location = 0) out vec4 FragColor;
-
-layout(early_fragment_tests) in;
+const vec3 KeyColor_LAB = vec3(21.0486,-5.2067,21.8668); //Modify as you wish in LAB format this code is between brown/green/yellow
 
 void main()
 {
-    vec4 color = SampleVideoTexture();
-    color.a = all(lessThan(color.rgb, key_color)) ? AlphaValue : 1.0f;
-    FragColor = color;
+  
+    vec4 sampleRGB = SampleVideoTexture();
+    vec3 sampleLAB = RGB_TO_LAB(sampleRGB.rgb);
+    float deltaE = LAB_DELTA_E_CIE2000(sampleLAB, KeyColor_LAB);
+    sampleRGB.a -= when_lt(deltaE, 10.0);   //tolerance, try 10 for instance
+    FragColor = sampleRGB;
 }
